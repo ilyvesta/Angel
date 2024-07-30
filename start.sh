c2c8a4c780d40e0f26ec3ee8ec39a666196f0867
ilyvesta <toptiermartin@gmail.com>
2024-07-30 07:56:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ilyvesta/hey_tess.git /hey_tess
+  git clone https://github.com/ilyvesta/Angel.git /Angel
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /hey_tess
+  git clone $UPSTREAM_REPO /Angel 
 fi
 cd /hey_tess
 pip3 install -U -r requirements.txt
-echo "Starting hey_tess...."
+echo "Starting Angel...."
 python3 bot.py
